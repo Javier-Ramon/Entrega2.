ef7b56e73479d7623d925c75d39aeaeaae81eaef
Javier Ramón <javierignacioramon99@gmail.com>
2023-09-23 12:10:35 -0300

    guardo

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ const frutas = [
     { nombre: 'sandía', descripcion: 'Es una gran fruta verde y jugosa, perfecta para el verano.', imagen: '../recursos/sandia.avif' },
     { nombre: 'mandarina', descripcion: 'Tiene forma esférica, contiene una pulpa dulce y jugosa que se divide en 10 ó 12 gajos.', imagen: '../recursos/mandarina.jpg' },
     { nombre: 'peras', descripcion: 'Es una fruta en forma de bombilla; su cáscara es lisa, la cual puede ser de color verde, amarillo, café o rojizo. La pulpa es blanca y jugosa.', imagen: '../recursos/peras.jpg' },
+    { nombre: 'kiwi', descripcion: ' Su piel es de color marrón oscuro, y encierra una pulpa verde y jugosa, de sabor ácido muy agradable.', imagen: '../recursos/kiwi.jpg' },
+    { nombre: 'pomelo', descripcion: ' Sabor amargo, que contienen pulpa de color que va del amarillo pálido al rojo muy intenso. ', imagen: '../recursos/pomelo.jpg' },
+    { nombre: 'frutilla', descripcion: ' Es una planta perenne de la familia de las Rosáceas, cuyo fruto es comestible.', imagen: '../recursos/frutilla.jpg' },
 
 ];
   
